266350c7456ba96ffade7b1fa5d969fee5a52341
seannxh <nohsean6@gmail.com>
2025-04-24 21:46:39 -0500

    Login Controller

--- src/main/java/com/firstproject/springboot/firstwebapp/login/LoginController.java
+++ src/main/java/com/firstproject/springboot/firstwebapp/login/LoginController.java
@@ -2,7 +2,9 @@ package com.firstproject.springboot.firstwebapp.login;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,10 +12,15 @@ public class LoginController {
 
     @RequestMapping("/login")
     @ResponseBody
-    public String login() {
-        return "Welcome TO Login Page!";
+    public String login(@RequestParam(required=false) String name, ModelMap model) {
+        model.put("name", name);
+        return "Welcome to Login Page!" + name;
     }
 
+    @RequestMapping("/")
+    public String login1() {
+        return "Welcome to Login Page!";
+    }
 
 
 
